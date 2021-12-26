d7518096b9152f135a0715bb1239ea7d7fa5041c
KunigiMohan <kmohanr5@gmail.com>
2021-12-26 18:02:42 +0530

    Added : Using Lobok Library for Logging.

--- src/main/java/com/application/AddressBookAPP/AddressBookAppApplication.java
+++ src/main/java/com/application/AddressBookAPP/AddressBookAppApplication.java
@@ -1,15 +1,21 @@
 package com.application.AddressBookAPP;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 /**
  * @SpringBootApplication : Spring boot main application class.
  */
+@Slf4j
 @SpringBootApplication
 public class AddressBookAppApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		ApplicationContext context=SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book Application Started {} in Environment",context.getEnvironment().getProperty("environment"));
 	}
 }
