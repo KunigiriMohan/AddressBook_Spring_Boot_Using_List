8d7033e2d6bff719be8b2c1293351821cae1bc2e
KunigiMohan <kmohanr5@gmail.com>
2021-12-28 21:26:08 +0530

    Added : Modified Contact Data model.

--- src/main/java/com/application/AddressBookAPP/model/ContactData.java
+++ src/main/java/com/application/AddressBookAPP/model/ContactData.java
@@ -2,35 +2,36 @@ package com.application.AddressBookAPP.model;
 
 import com.application.AddressBookAPP.dto.ContactDTO;
 
-public class ContactData {
-    private int iD;
-    private ContactDTO contactDTO;
+import lombok.Data;
+/**
+ * @Data : To Generate Getters and Setters
+ */
+
+public @Data class ContactData {
+    
+    private int contactId;
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String pinCode;
+    private String mobileNo;
+    private String email;
+    public ContactData(){}
     
     /**
      * Defining Constructer
      * @param iD
      * @param contactDTO
      */
-    public ContactData(int iD, ContactDTO contactDTO) {
-        this.iD = iD;
-        this.contactDTO = contactDTO;
+    public ContactData(int contactId, ContactDTO contactDTO) {
+        this.contactId= contactId;
+        this.name=contactDTO.name;
+        this.address=contactDTO.address;
+        this.city=contactDTO.city;
+        this.state=contactDTO.state;
+        this.pinCode=contactDTO.pinCode;
+        this.mobileNo=contactDTO.mobileNo;
+        this.email=contactDTO.email;
     }
-    /**
-    * Getters and Setters
-    */
-    public int getiD() {
-        return iD;
-    }
-
-    public void setiD(int iD) {
-        this.iD = iD;
-    }
-
-    public ContactDTO getContactDTO() {
-        return contactDTO;
-    }
-
-    public void setContactDTO(ContactDTO contactDTO) {
-        this.contactDTO = contactDTO;
-    }  
 }
