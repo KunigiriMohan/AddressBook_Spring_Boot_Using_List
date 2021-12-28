792efe22a20a97c760fc8f0b6e18746977f24afe
KunigiMohan <kmohanr5@gmail.com>
2021-12-28 21:22:04 +0530

    Added : Adding Validation to Details.

--- src/main/java/com/application/AddressBookAPP/dto/ContactDTO.java
+++ src/main/java/com/application/AddressBookAPP/dto/ContactDTO.java
@@ -1,43 +1,32 @@
 package com.application.AddressBookAPP.dto;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-
-import lombok.Data;
+import lombok.ToString;
 
 /**
  * @Data : Using Lombok to auto Generate Getters and Setters
  */
-public @Data class  ContactDTO {
+public @ToString class  ContactDTO {
 
     @Pattern(regexp= "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "C0ntact name is not valid")
-    private String name;
-    private String address;
-    private String city;
-    private String state;
-    @Pattern(regexp = "^[1-9]{1}[0-9]{5}$",message = "Entered Wrong Pincode")
-    private String pinCode;
-    @Pattern(regexp = "^[6789]{1}[0-9]{9}",message = "Entered wrong mobile Number")
-    private String mobileNo;
-    private String email;
+    public String name;
     
-    /**
-     * Defining Constructer
-     * @param name
-     * @param address
-     * @param city
-     * @param state
-     * @param pinCode
-     * @param mobileNo
-     * @param email
-     */
-    public ContactDTO( String name, String address, String city, String state, String pinCode, String mobileNo,
-            String email) {
-        this.name = name;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.pinCode = pinCode;
-        this.mobileNo = mobileNo;
-        this.email = email;
-    }
+    @NotNull(message = "Address Should not be Empty")
+    public String address;
+    
+    @NotNull(message = "City Should not Empty")
+    public String city;
+    
+    @NotNull(message = "State should not Empty")
+    public String state;
+    
+    @Pattern(regexp = "^[1-9]{1}[0-9]{5}$",message = "Entered Wrong Pincode")
+    public String pinCode;
+    
+    @Pattern(regexp = "^[6789]{1}[0-9]{9}",message = "Entered wrong mobile Number")
+    public String mobileNo;
+    
+    @NotNull(message = "Email Should not Empty")
+    public String email;
 }
